16d78b5cb876827eee98df66849a8dccb107485c
Eva-M-Lopez <eva.m.lopez2004@gmail.com>
2024-06-17 16:55:09 -0400

    Create start.py

--- Intro-To-Python/start.py
+++ Intro-To-Python/start.py
@@ -1,2 +1,9 @@
 #basic variable
-print("Hello World")
+my_str = "Hello World"
+my_int = 3
+print(f"{my_str} {my_int}")
+#Another way to format a string without having to concatonizing it
+#print(my_str + " " +  str(my_int))
+#how to cast int to string
+#print(my_int)
+
