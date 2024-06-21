f71778777a949d00115f9ed340bade1d80a53195
Eva-M-Lopez <eva.m.lopez2004@gmail.com>
2024-06-21 19:31:53 -0400

    making a list of dict and avg cal

--- Intro-To-Python/project.py
+++ Intro-To-Python/project.py
@@ -1,11 +1,40 @@
 import csv
+import numpy as np
 #read from file
-with open('Intro-To-Python/sleepdata.csv', 'r') as csvfile:
-    #create a reader object
-    csv_reader = csv.reader(csvfile)
-    header = next(csv_reader)
-    #print(header)
-    my_list = []
-    for row in csv_reader:
-        my_list.append(row)
-    print(my_list)
+
+def read():
+    with open('Intro-To-Python/weatherdata.csv', 'r') as csvfile:
+        #create a reader object
+        csv_reader = csv.DictReader(csvfile)
+        
+        #create a list of dictionaries - list comprehension
+        wdata = [row for row in csv_reader]
+        
+        return wdata
+ 
+
+def average_calculator(my_list: list):
+    temp_list = []
+    for item in my_list:
+        temp_list.append(int(item['Temperature']))
+    tl = np.array(temp_list)
+
+    temp_list2 = []
+    for item in my_list:
+        temp_list2.append(int(item['Humidity']))
+    tl2 = np.array(temp_list2)
+
+    print(np.mean(tl))
+    print(np.mean(tl2))
+    return
+
+
+average_calculator(read())
+
+
+
+
+
+
+
+
