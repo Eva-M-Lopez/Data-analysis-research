f31fa49252b5c64e9ed77d09214364e1d81d01eb
Eva-M-Lopez <eva.m.lopez2004@gmail.com>
2024-06-27 17:36:12 -0400

    fin pandas

--- Intro-To-Pandas/Intro-to-pandas-part1.py
+++ Intro-To-Pandas/Intro-to-pandas-part1.py
@@ -1,10 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+#import pandas and graph availability
+
 df = pd.read_csv("Intro-To-Pandas/weather_data.csv")
+#reader
+
 #print(df.head())
+#prints the top few entries
+
 #print(df[["Temperature (°C)", "Precipitation (mm)"]].mean())
+#got the mean of the temperature and precipitation
+
 #print(df.describe())
-#average the group by city
+#gives some general statistics
+
 #print(df[["Location", "Precipitation (mm)"]].groupby("Location").mean())
+#average the group by city
 
 def celsius_to_fahrenheit(temp: float):
     return (temp *(9/5))+32
@@ -15,8 +26,19 @@ def km_to_m(wind_speed: float):
 def mm_to_in(rain_fall: float):
     return rain_fall*0.03937
 
+#converts from metric to non-metric values
+
 df["Temperature (°F)"] = df["Temperature (°C)"].apply(celsius_to_fahrenheit)
 df["Wind Speed (mph)"] = df["Wind Speed (km/h)"].apply(km_to_m)
 df["Precipitation (in)"] = df["Precipitation (mm)"].apply(mm_to_in)
+#adds the non-metric information in
+
 df.drop(["Temperature (°C)","Wind Speed (km/h)","Precipitation (mm)"], axis=1, inplace=True)
+#gets rid of the metric information
+
 print(df)
+#print the information
+
+df["Temperature (°F)"].plot()
+#how to seperate the temperature
+plt.show()
